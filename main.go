8abea79c06c18b1553fa820c12365ff767821288
fummicc1 <fumiya.tennis1@gmail.com>
2019-09-01 18:39:13 +0900

    サーバー側

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/sideshow/apns2"
+	"github.com/sideshow/apns2/certificate"
+	"github.com/sideshow/apns2/payload"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World")
+	fmt.Printf(r.URL.RawQuery)
+}
+
+func httpRUn() {
+	http.HandleFunc("/location", handler) // ハンドラを登録してウェブページを表示させる
+	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+
+	cert, err := certificate.FromP12File("pusher.p12", "")
+
+	fmt.Printf("start.")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("cert created.")
+	notification := &apns2.Notification{}
+	notification.DeviceToken = "2c00793251098ed2349830f8f3474167ec903d2b4b1a4dd65a7c900803c08028"
+	notification.Topic = "com.fumiya.LocationShareAppWithAPNS"
+	notification.Payload = payload.NewPayload().ContentAvailable()
+	client := apns2.NewClient(cert).Development()
+	res, err := client.Push(notification)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%v %v %v\n", res.StatusCode, res.ApnsID, res.Reason)
+
+	httpRUn()
+}
